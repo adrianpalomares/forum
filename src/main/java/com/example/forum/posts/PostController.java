e99423c3a86cb65995b6484e48dd94d3b626091e
Adrian Palomares <adrianpal45@gmail.com>
2021-01-03 17:58:13 -0800

    Added more mappings

--- src/main/java/com/example/forum/posts/PostController.java
+++ src/main/java/com/example/forum/posts/PostController.java
@@ -1,8 +1,10 @@
 package com.example.forum.posts;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -10,9 +12,36 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
-    @GetMapping("/posts")
+    @GetMapping("api/posts/{id}")
+    public Optional<Post> getPostById(@PathVariable(value = "id") Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    @GetMapping("api/posts")
     public Iterable<Post> getAllPosts() {
         return postRepository.findAll();
+    }
+
+    @PostMapping("api/posts")
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping("api/posts/{id}")
+    public Post updatePost(@PathVariable(value = "id") Long postId, @RequestBody Post postDetails) {
+        Optional<Post> post = postRepository.findById(postId);
+        // Handle if null
+        post.get().setTitle(postDetails.getTitle());
+        Post updatedPost = postRepository.save(post.get());
+        return updatedPost;
+
+    }
+
+    @DeleteMapping("api/posts/{id}")
+    public Post deletePost(@PathVariable(value = "id") Long postId) {
+        Optional<Post> post = postRepository.findById(postId);
+        postRepository.delete(post.get());
+        return post.get();
     }
 
 }
