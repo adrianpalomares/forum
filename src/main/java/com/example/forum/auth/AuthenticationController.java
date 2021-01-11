09e1addbfcbd114f82158d959f350368d2ff330f
Adrian Palomares <adrianpal45@gmail.com>
2021-01-10 21:05:11 -0800

    added ability to register users

--- src/main/java/com/example/forum/auth/AuthenticationController.java
+++ src/main/java/com/example/forum/auth/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.example.forum.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +15,12 @@ public class AuthenticationController {
     @PostMapping("api/auth/login")
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         return authenticationService.login(loginRequest);
+    }
+
+    @PostMapping("api/auth/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
+        RegisterResponse registerResponse = authenticationService.register(registerRequest);
+        ResponseEntity<RegisterResponse> response = new ResponseEntity<>(registerResponse, HttpStatus.CREATED);
+        return new ResponseEntity<>(registerResponse, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/forum/auth/AuthenticationService.java
+++ src/main/java/com/example/forum/auth/AuthenticationService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -16,9 +17,28 @@ public class AuthenticationService {
     @Autowired
     JwtUtil jwtUtil;
 
-    public LoginResponse login(LoginRequest loginRequest) throws UsernameNotFoundException{
+    public LoginResponse login(LoginRequest loginRequest) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(loginRequest.getUsername());
         user.orElseThrow(() -> new UsernameNotFoundException("Username doesn't exist."));
         return new LoginResponse(jwtUtil.generateToken(user.get().getUsername()));
+    }
+
+    // TODO: Throw exception when user exists
+    public RegisterResponse register(RegisterRequest registerRequest) {
+        // Get user if it exists return error code
+        Optional<User> user = userRepository.findByUsername(registerRequest.getUsername());
+        System.out.println(user.isPresent());
+        if (!user.isPresent()) {
+            // Create new user
+            User newUser = new User();
+            newUser.setUsername(registerRequest.getUsername());
+            newUser.setPassword(registerRequest.getPassword());
+            newUser.setEmail(registerRequest.getEmail());
+            newUser.setCreated(Instant.now());
+            User savedUser = userRepository.save(newUser);
+
+            return new RegisterResponse(jwtUtil.generateToken(savedUser.getUsername()));
+        }
+        return new RegisterResponse(null);
     }
 }
