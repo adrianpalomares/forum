6d81b9e6ca8bd66a7638dd32cfbd45e6ce8f4576
Adrian Palomares <adrianpal45@gmail.com>
2021-01-10 17:15:20 -0800

    added layout for register page

--- src/main/resources/frontend/angular-app/src/app/pages/register/register.component.ts
+++ src/main/resources/frontend/angular-app/src/app/pages/register/register.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    constructor(private fb: FormBuilder, private authService: AuthService) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.form = this.fb.group({
 
-  ngOnInit(): void {
-  }
-
+        });
+    }
 }
