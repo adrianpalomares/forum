757a0862151e75ee30c55e4de4667c8ceb6d5777
Adrian Palomares <adrianpal45@gmail.com>
2021-01-14 16:13:34 -0800

    allowing requests to /api/auth/register

--- src/main/java/com/example/forum/security/SecurityConfiguration.java
+++ src/main/java/com/example/forum/security/SecurityConfiguration.java
@@ -15,7 +15,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/api/auth/login", "/api/posts").permitAll().
+                .authorizeRequests().antMatchers("/api/auth/login", "/api/posts", "/api/auth/register").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
