de289a3c1299a3ca4c78c35f43b9423f4a78d5f2
Adrian Palomares <adrianpal45@gmail.com>
2021-01-18 12:13:22 -0800

    added comment import

--- src/main/resources/frontend/angular-app/src/app/posts/postCard.component.ts
+++ src/main/resources/frontend/angular-app/src/app/posts/postCard.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommentService } from '../comments/comments.service';
 import { Post } from './post.model';
+import { Comment } from '../comments/comment.model';
 
 @Component({
     selector: 'app-postcard',
