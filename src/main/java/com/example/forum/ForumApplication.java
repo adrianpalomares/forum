50c2ac512ff6811a1998ad3b4d2931bb8d337076
Adrian Palomares <adrianpal45@gmail.com>
2021-02-12 18:18:24 -0800

    no longer serves angular app

--- src/main/java/com/example/forum/ForumApplication.java
+++ src/main/java/com/example/forum/ForumApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import org.springframework.core.io.Resource;
+
 import java.io.IOException;
 
 @SpringBootApplication
@@ -20,22 +21,22 @@ public class ForumApplication {
 
 }
 
-
-@Configuration
-class MvcConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/public/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/public/index.html");
-                    }
-                });
-    }
-}
+/**
+ * // For serving angular app (moved to different repo)
+ *
+ * @Configuration class MvcConfiguration implements WebMvcConfigurer {
+ * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+ * registry.addResourceHandler("/**")
+ * .addResourceLocations("classpath:/public/")
+ * .resourceChain(true)
+ * .addResolver(new PathResourceResolver() {
+ * @Override protected Resource getResource(String resourcePath, Resource location) throws IOException {
+ * Resource requestedResource = location.createRelative(resourcePath);
+ * <p>
+ * return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+ * : new ClassPathResource("/public/index.html");
+ * }
+ * });
+ * }
+ * }
+ */
