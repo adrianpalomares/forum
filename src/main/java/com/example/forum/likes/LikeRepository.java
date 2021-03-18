4b7c7aa83f6568ef467c1500efd78b48bd4dca91
Adrian Palomares <adrianpal45@gmail.com>
2021-03-17 17:39:41 -0700

    Added new method to like repository

--- src/main/java/com/example/forum/likes/LikeRepository.java
+++ src/main/java/com/example/forum/likes/LikeRepository.java
@@ -1,6 +1,7 @@
 package com.example.forum.likes;
 
 import com.example.forum.posts.Post;
+import com.example.forum.users.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface LikeRepository extends CrudRepository<Like, Long> {
     // Grab likes by post
     List<Like> findByPost(Post post);
+
+    // Query for a like by Post and User
+    Like findByPostAndUser(Post post, User user);
 }
