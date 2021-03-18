53704d947b1316063f547b3069e45c3c50eaff59
Adrian Palomares <adrianpal45@gmail.com>
2021-03-17 20:25:20 -0700

    added ways to get likes from posts endpoint

--- src/main/java/com/example/forum/posts/PostController.java
+++ src/main/java/com/example/forum/posts/PostController.java
@@ -1,8 +1,11 @@
 package com.example.forum.posts;
 
+import com.example.forum.likes.Like;
+import com.example.forum.likes.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 //TODO: Create a ResponseObject for post
 
@@ -11,6 +14,9 @@ import java.util.Optional;
 public class PostController {
     @Autowired
     PostService postService;
+
+    @Autowired
+    LikeService likeService;
 
     @CrossOrigin(origins = "*")
     @GetMapping("api/posts/{id}")
@@ -22,6 +28,19 @@ public class PostController {
     @GetMapping("api/posts")
     public Iterable<Post> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    /**
+     * Grab the likes from a post.
+     *
+     * @return A List of likes.
+     */
+    @CrossOrigin(origins = "*")
+    @GetMapping("api/posts/{id}/likes")
+    public List<Like> getLikesFromPost(@PathVariable(value = "id") Long postId) {
+        // Grab likes using post service
+        List<Like> listOfLikes = likeService.getLikesByPost(postId);
+        return listOfLikes;
     }
 
     @PostMapping("api/posts")
