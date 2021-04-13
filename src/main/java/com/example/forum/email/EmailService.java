f49ca4cbb3255659957a674f52ff789c410a270d
Adrian Palomares <adrianpal45@gmail.com>
2021-04-12 18:01:53 -0700

    simple registration email

--- src/main/java/com/example/forum/email/EmailService.java
+++ src/main/java/com/example/forum/email/EmailService.java
@@ -1,9 +1,21 @@
 package com.example.forum.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
+
+    public void sendRegistrationEmail(String toEmailAddress) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("Forum<apalom22@gmail.com>");
+        simpleMailMessage.setTo(toEmailAddress);
+        simpleMailMessage.setSubject("Welcome to Forum!");
+        simpleMailMessage.setText("This is the registration email.");
+        javaMailSender.send(simpleMailMessage);
+    }
 }
