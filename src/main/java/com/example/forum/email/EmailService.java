e9b1b55d3553ba5ef2c93ecf606b327a91fe1be2
Adrian Palomares <adrianpal45@gmail.com>
2021-04-13 11:24:08 -0700

    Sends email on registration

--- src/main/java/com/example/forum/email/EmailService.java
+++ src/main/java/com/example/forum/email/EmailService.java
@@ -10,12 +10,12 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendRegistrationEmail(String toEmailAddress) {
+    public void sendRegistrationEmail(String toEmailAddress, String username, String password) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom("Forum<apalom22@gmail.com>");
         simpleMailMessage.setTo(toEmailAddress);
         simpleMailMessage.setSubject("Welcome to Forum!");
-        simpleMailMessage.setText("This is the registration email.");
+        simpleMailMessage.setText("Here are your credentials:\n\nUsername: " + username + "\nPassword: " + password);
         javaMailSender.send(simpleMailMessage);
     }
 }
